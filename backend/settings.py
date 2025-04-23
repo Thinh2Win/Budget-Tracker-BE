5ebe0815f15a57efd9c1b3e062a10f41037f38a0
Thinh Ngo Phan <ngophanthinh@gmail.com>
2025-04-23 16:11:39 -0700

    backend configurations: urls, cors, rest_framework

--- backend/settings.py
+++ backend/settings.py
@@ -41,9 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+  os.getenv('REACT_SERVER'),   # your React dev server
+]
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
+}
